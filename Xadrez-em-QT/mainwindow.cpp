57ca7f16d1946f024dd0381f1287d10a7148f333
gabriellgpc <gabriellgpc@hotmail.com>
2018-01-25 23:29:39 -0300

    Tab 0.1.1

--- Xadrez-em-QT/mainwindow.cpp
+++ Xadrez-em-QT/mainwindow.cpp
@@ -2,7 +2,7 @@
 #include "ui_mainwindow.h"
 
 #include <QPainter>
-#include <QPixmap>
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -13,7 +13,7 @@ MainWindow::MainWindow(QWidget *parent) :
 
     for(int i = 0; i < 8; i++)for(int j = 0; j < 8; j++)
     {
-       ui->tabuleiro->setCellWidget(i,j,new QPixmap);
+       ui->tabuleiro->setCellWidget(i,j,new );
     }
 
 }
