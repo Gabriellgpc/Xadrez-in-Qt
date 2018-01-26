d325b9d6502e355889a07f389b525f36e8646d49
gabriellgpc <gabriellgpc@hotmail.com>
2018-01-25 23:25:45 -0300

    Tab 0.1

--- Xadrez-em-QT/mainwindow.cpp
+++ Xadrez-em-QT/mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPainter>
+#include <QPixmap>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    for(int i = 0; i < 8; i++)for(int j = 0; j < 8; j++)
+    {
+       ui->tabuleiro->setCellWidget(i,j,new QPixmap);
+    }
+
 }
 
 MainWindow::~MainWindow()
--- build-Xadrez-em-QT-Desktop_Qt_5_9_1_MinGW_32bit-Debug/ui_mainwindow.h
+++ build-Xadrez-em-QT-Desktop_Qt_5_9_1_MinGW_32bit-Debug/ui_mainwindow.h
@@ -13,13 +13,18 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -28,8 +33,17 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
-    QGridLayout *gridLayout;
     QTableWidget *tabuleiro;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout_2;
+    QLabel *label;
+    QTableView *tableViewHistorico;
+    QWidget *widget1;
+    QVBoxLayout *verticalLayout;
+    QTableView *viewChat;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *ButtonEnviar;
+    QLineEdit *lineEditMensagem;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -38,43 +52,96 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(638, 496);
+        MainWindow->resize(740, 600);
+        MainWindow->setMinimumSize(QSize(740, 600));
+        MainWindow->setMaximumSize(QSize(740, 600));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        gridLayout = new QGridLayout(centralWidget);
-        gridLayout->setSpacing(6);
-        gridLayout->setContentsMargins(11, 11, 11, 11);
-        gridLayout->setObjectName(QStringLiteral("gridLayout"));
         tabuleiro = new QTableWidget(centralWidget);
         if (tabuleiro->columnCount() < 8)
             tabuleiro->setColumnCount(8);
         if (tabuleiro->rowCount() < 8)
             tabuleiro->setRowCount(8);
         tabuleiro->setObjectName(QStringLiteral("tabuleiro"));
-        tabuleiro->setBaseSize(QSize(0, 0));
-        tabuleiro->viewport()->setProperty("cursor", QVariant(QCursor(Qt::PointingHandCursor)));
-        tabuleiro->setAutoScroll(false);
-        tabuleiro->setTabKeyNavigation(false);
-        tabuleiro->setShowGrid(true);
-        tabuleiro->setWordWrap(false);
-        tabuleiro->setCornerButtonEnabled(false);
+        tabuleiro->setGeometry(QRect(0, 0, 500, 500));
+        tabuleiro->setMinimumSize(QSize(500, 500));
+        tabuleiro->setMaximumSize(QSize(500, 500));
         tabuleiro->setRowCount(8);
         tabuleiro->setColumnCount(8);
         tabuleiro->horizontalHeader()->setVisible(false);
-        tabuleiro->horizontalHeader()->setDefaultSectionSize(200);
+        tabuleiro->horizontalHeader()->setDefaultSectionSize(62);
         tabuleiro->horizontalHeader()->setHighlightSections(false);
-        tabuleiro->horizontalHeader()->setMinimumSectionSize(200);
+        tabuleiro->horizontalHeader()->setMinimumSectionSize(62);
         tabuleiro->verticalHeader()->setVisible(false);
-        tabuleiro->verticalHeader()->setDefaultSectionSize(200);
+        tabuleiro->verticalHeader()->setDefaultSectionSize(62);
         tabuleiro->verticalHeader()->setHighlightSections(false);
-        tabuleiro->verticalHeader()->setMinimumSectionSize(200);
+        tabuleiro->verticalHeader()->setMinimumSectionSize(62);
+        widget = new QWidget(centralWidget);
+        widget->setObjectName(QStringLiteral("widget"));
+        widget->setGeometry(QRect(520, 20, 213, 205));
+        verticalLayout_2 = new QVBoxLayout(widget);
+        verticalLayout_2->setSpacing(6);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(widget);
+        label->setObjectName(QStringLiteral("label"));
+        QFont font;
+        font.setPointSize(10);
+        label->setFont(font);
+        label->setAlignment(Qt::AlignCenter);
 
-        gridLayout->addWidget(tabuleiro, 0, 0, 1, 1);
+        verticalLayout_2->addWidget(label);
+
+        tableViewHistorico = new QTableView(widget);
+        tableViewHistorico->setObjectName(QStringLiteral("tableViewHistorico"));
+        tableViewHistorico->setMinimumSize(QSize(211, 181));
+        tableViewHistorico->setMaximumSize(QSize(211, 181));
+
+        verticalLayout_2->addWidget(tableViewHistorico);
+
+        widget1 = new QWidget(centralWidget);
+        widget1->setObjectName(QStringLiteral("widget1"));
+        widget1->setGeometry(QRect(520, 240, 216, 261));
+        verticalLayout = new QVBoxLayout(widget1);
+        verticalLayout->setSpacing(6);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        viewChat = new QTableView(widget1);
+        viewChat->setObjectName(QStringLiteral("viewChat"));
+        viewChat->setMinimumSize(QSize(214, 228));
+        viewChat->setMaximumSize(QSize(214, 228));
+
+        verticalLayout->addWidget(viewChat);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        ButtonEnviar = new QPushButton(widget1);
+        ButtonEnviar->setObjectName(QStringLiteral("ButtonEnviar"));
+
+        horizontalLayout->addWidget(ButtonEnviar);
+
+        lineEditMensagem = new QLineEdit(widget1);
+        lineEditMensagem->setObjectName(QStringLiteral("lineEditMensagem"));
+
+        horizontalLayout->addWidget(lineEditMensagem);
+
+
+        verticalLayout->addLayout(horizontalLayout);
 
         MainWindow->setCentralWidget(centralWidget);
+        tabuleiro->raise();
+        tableViewHistorico->raise();
+        label->raise();
+        viewChat->raise();
+        lineEditMensagem->raise();
+        ButtonEnviar->raise();
+        tableViewHistorico->raise();
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 638, 21));
+        menuBar->setGeometry(QRect(0, 0, 740, 21));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -91,6 +158,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        label->setText(QApplication::translate("MainWindow", "Historico", Q_NULLPTR));
+        ButtonEnviar->setText(QApplication::translate("MainWindow", "enviar", Q_NULLPTR));
     } // retranslateUi
 
 };
